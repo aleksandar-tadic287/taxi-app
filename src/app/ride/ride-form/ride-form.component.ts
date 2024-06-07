75e13668817b8b09f97797597828263cfe1c35f2
aleksandar-tadic287 <atadic287@gmail.com>
2024-06-07 06:58:26 +0200

    Add Ride Form page content

--- src/app/ride/ride-form/ride-form.component.ts
+++ src/app/ride/ride-form/ride-form.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ride-form',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './ride-form.component.html',
   styleUrl: './ride-form.component.scss'
 })
-export class RideFormComponent {
+export class RideFormComponent implements OnInit {
+  public rideForm: FormGroup;
+  public paymentMethod!: string;
+  public formSubmitted!: boolean;
 
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.rideForm = this.fb.group({
+      startingAdress: ['', Validators.required],
+      finalAddress: ['', Validators.required],
+      dateTime: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      paymentMethod: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    this.formSubmitted = false;
+  }
+
+  changePaymentMethod(event: Event): void {
+    this.paymentMethod = (event.target as HTMLInputElement).value;
+    this.formSubmitted = false;
+
+    if (this.paymentMethod === 'card') {
+      this.rideForm.addControl('cardNumber', new FormControl('', Validators.required));
+      this.rideForm.addControl('expirationCardDate', new FormControl('', Validators.required));
+    } else {
+      this.rideForm.removeControl('cardNumber');
+      this.rideForm.removeControl('expirationCardDate');
+    }
+  }
+
+  submitForm(): void {
+    console.log('Form submitted', this.rideForm.value);
+  }
 }
