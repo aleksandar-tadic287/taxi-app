25f841a7da97bd2b2f5e4a3b528884d670291f11
aleksandar-tadic287 <atadic287@gmail.com>
2024-06-07 08:27:00 +0200

    Display scheduled rides as a list on a separate page

--- src/app/ride/ride-list/ride-list.component.ts
+++ src/app/ride/ride-list/ride-list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ScheduledRide } from '../../core/models/scheduled-ride.model';
+import { SharedService } from '../../core/services/shared-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ride-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './ride-list.component.html',
   styleUrl: './ride-list.component.scss'
 })
-export class RideListComponent {
+export class RideListComponent implements OnInit {
+  public scheduledRideList!: ScheduledRide[];
 
+  constructor(
+    private sharedService: SharedService
+  ) {}
+
+  ngOnInit(): void {
+    this.scheduledRideList = this.sharedService.getScheduledRideList();
+  }
 }
