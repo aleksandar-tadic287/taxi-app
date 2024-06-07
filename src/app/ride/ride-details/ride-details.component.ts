0ac04e7eedd06e4ac2c37866fb2dbe5415409786
aleksandar-tadic287 <atadic287@gmail.com>
2024-06-07 09:12:56 +0200

    Display specific Ride details in a separate page

--- src/app/ride/ride-details/ride-details.component.ts
+++ src/app/ride/ride-details/ride-details.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../../core/services/shared-service';
+import { ActivatedRoute } from '@angular/router';
+import { ScheduledRide } from '../../core/models/scheduled-ride.model';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-ride-details',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrencyPipe,
+    DatePipe
+  ],
   templateUrl: './ride-details.component.html',
   styleUrl: './ride-details.component.scss'
 })
-export class RideDetailsComponent {
+export class RideDetailsComponent implements OnInit {
+  public scheduledRide!: ScheduledRide;
+  public scheduledRideList: ScheduledRide[] = [];
+  private rideId!: number;
 
+  constructor(
+    private sharedService: SharedService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.scheduledRideList = this.sharedService.getScheduledRideList();
+
+    if (this.scheduledRideList.length) {
+      this.rideId = Number(this.route.snapshot.paramMap.get('id'));
+      this.scheduledRide = this.scheduledRideList[this.rideId];
+    }
+  }
 }
--- src/app/ride/ride-list/ride-list.component.ts
+++ src/app/ride/ride-list/ride-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ScheduledRide } from '../../core/models/scheduled-ride.model';
 import { SharedService } from '../../core/services/shared-service';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ride-list',
@@ -16,10 +17,15 @@ export class RideListComponent implements OnInit {
   public scheduledRideList!: ScheduledRide[];
 
   constructor(
-    private sharedService: SharedService
+    private sharedService: SharedService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.scheduledRideList = this.sharedService.getScheduledRideList();
+  }
+
+  showRideDetails(index: number): void {
+    this.router.navigate(['/ride-list', index]);
   }
 }
