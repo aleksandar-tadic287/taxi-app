790c882284593942ddf3daf98856b9d4182d7b0f
aleksandar-tadic287 <atadic287@gmail.com>
2024-06-07 06:15:50 +0200

    Add basic component blocks and functional routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'route-form'
+  },
+  {
+    path: 'ride-form',
+    loadComponent: () => import('./ride/ride-form/ride-form.component')
+      .then(m => m.RideFormComponent)
+  },
+  {
+    path: 'ride-list',
+    loadComponent: () => import('./ride/ride-list/ride-list.component')
+      .then(m => m.RideListComponent)
+  },
+  {
+    path: 'ride-list/:id',
+    loadComponent: () => import('./ride/ride-details/ride-details.component')
+      .then(m => m.RideDetailsComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./ride/ride-form/ride-form.component')
+      .then(m => m.RideFormComponent)
+  }
+];
